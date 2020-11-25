839a1d7a0c089a3c205d680f5cfa8a8f7d310648
Fabio Gallonetto <fabio.gallonetto@imaginecurve.com>
2020-11-25 16:53:41 +0000

    Initial Commit

---
+++ SwiftBoy/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  SwiftBoy
+//
+//  Created by Fabio Gallonetto on 25/11/2020.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    @IBOutlet var window: NSWindow!
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        
+    }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+    
+}
